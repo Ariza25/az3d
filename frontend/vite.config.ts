d0f93daf96a0a32b727f999237a37cf7f934abdb
Matheus Ariza <mjsa.sanches25@gmail.com>
2026-08-29 11:40:09 -0300

    configure GitHub Pages for custom domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: true,
+    },
+  };
 });
